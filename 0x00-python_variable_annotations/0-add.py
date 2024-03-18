65192c6055b661f6dd135c079ae42353ec04688b
Mercy Muthoni <ndirangumercy4@gmail.com>
2024-03-18 11:08:54 +0000

    python backend

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+type annotated function add
+"""
+
+
+def add(a: float, b:float) -> float:
+    """type annotated a and b and adding them"""
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+a type annotated concat that returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ aiming to return the concatanated str1 and str2"""
+    return str1 + str2
