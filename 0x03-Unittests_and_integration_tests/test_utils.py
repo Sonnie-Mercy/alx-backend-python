95c05ee71ddb74bbde978e86bddf770f475f5aa3
Mercy Muthoni <ndirangumercy4@gmail.com>
2024-05-13 18:36:34 +0000

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,9 +20,9 @@ class TestAccessNestedMap(unittest.TestCase):
     the TestAcessNestedMap class
     """
     @parameterized.expand([
-        ({"a": 1}, ("a"), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": 1}, ['a'], 1),
+        ({"a": {"b": 2}}, ['a'], {"b": 2}),
+        ({"a": {"b": 2}}, ['a', 'b'], 2)
     ])
     def test_access_nested_map(self, nested_map: Mapping,
                                path: Sequence, expected_result: Any) -> None:
@@ -32,8 +32,8 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
     @parameterized.expand([
-        ({}, ("a",), "KeyError: 'a'"),
-        ({"a": 1}, ("a", "b"), "KeyError: 'b'")
+        ({}, ['a']),
+        ({"a": 1}, ['a', 'b'])
     ])
     def test_access_nested_map_exceptions(self, nested_map: Mapping,
                                           path: Sequence) -> None:
@@ -42,6 +42,25 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    the TestGetJson class
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        the test_get_json function
+        """
+        with patch('utils.requests.get') as mock_request_get:
+            mock_request_get.return_value.json.return_value = test_payload
+            resp = get_json(test_url)
+            mock_request_get.assert_called_with(test_url)
+            self.assertEqual(resp, test_payload)
 
 
 if __name__ == '__main__':
